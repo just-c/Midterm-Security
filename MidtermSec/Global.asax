713c9f71e62d7c2d4880bda653f2bff5e1a32df2
justinward25 <wjustin1994@gmail.com>
2015-07-02 15:17:35 -0400

    initial commit

---
+++ MidtermSec/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MidtermSec.MvcApplication" Language="C#" %>
---
+++ MidtermSec/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CourseCode] NCHAR(10) NULL, 
+    [CourseName] NCHAR(10) NULL, 
+    [CourseDesc] NCHAR(10) NULL, 
+    [CourseCost] NCHAR(10) NULL
+)
